baeee60105047700d5efe4bf98b870a6fc193353
Matthew McNee <matthew.mcnee@student.manchester.ac.uk>
2023-10-31 14:47:37 +0000

    working 2d flappy bird

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import CreateSheet from './create-sheet/CreateSheet.jsx';
 import ChartLayout from './Chart';
 import Quiz from './Quiz';
 import Bird from './three/Bird';
-import Text from './three/Text';
+// import Text from './three/Text';
 import './App.css'
 
 const App = ({onTitleString}) => {
@@ -21,7 +21,7 @@ const App = ({onTitleString}) => {
         <Route path="/chart" element={<ChartLayout setTitle={updateTitleString}/>} />
         <Route path="/quiz" element={<Quiz setTitle={updateTitleString}/>} />
         <Route path="/bird" element={<Bird setTitle={updateTitleString}/>} />
-        <Route path="/text" element={<Text setTitle={updateTitleString}/>} />
+        <Route path="/text" element={<Bird setTitle={updateTitleString}/>} />
       </Routes>
     </Router>
   );
--- src/three/Bird.jsx
+++ src/three/Bird.jsx
@@ -2,15 +2,15 @@ import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import './text.css'
 
 const Bird = ({setTitle}) => {
   setTitle("Edu Homepage");
   const canvasRef = useRef();
   const flappyBirdContainerRef = useRef(null);
+  var begun;
+
   useEffect(() => {
-
-
-
 
     let questions = [
     {"question": ["What is 4 * 5?", "20", "18", "22", "25"]},
@@ -24,6 +24,13 @@ const Bird = ({setTitle}) => {
     {"question": ["Solve for y: 2y - 10 = 14", "12", "10", "16", "14"]},
     {"question": ["Calculate the perimeter of a square with side length 10", "40", "36", "44", "38"]}
 ]
+
+    // Very disgusting solution to hide double render of labels in react strict mode
+    var opacity = 1;
+    if(begun == true){
+      opacity = 0;
+    }
+    begun = true;
 
 function shuffleAnswers(questionObj) {
     const question = questionObj.question[0];
@@ -46,81 +53,68 @@ function shuffleAnswers(questionObj) {
         answer: correctAnswer
     };
 }
-
-console.log("hello")
 runGame();
 
 function runGame(){
   // Set up Three.js scene
-    var scene = new THREE.Scene();
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    var aspectRatio = width / height;
-    var camera = new THREE.OrthographicCamera(-aspectRatio, aspectRatio, 1, -1, 1, 1000);
-    camera.position.z = 2;
-    var renderer = new THREE.WebGLRenderer({ alpha: true, canvas: canvasRef.current  });
-    renderer.setSize(width, height);
-    const flappyBirdContainer = flappyBirdContainerRef.current;
-    flappyBirdContainer.appendChild(renderer.domElement);
+  var scene = new THREE.Scene();
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var aspectRatio = width / height;
+  var camera = new THREE.OrthographicCamera(-aspectRatio, aspectRatio, 1, -1, 1, 1000);
+  camera.position.z = 2;
+  var renderer = new THREE.WebGLRenderer({ alpha: true, canvas: canvasRef.current  });
+  renderer.setSize(width, height);
+  const flappyBirdContainer = flappyBirdContainerRef.current;
+  flappyBirdContainer.appendChild(renderer.domElement);
 
 
-    // Update camera and renderer on screen resize
-    window.addEventListener('resize', () => {
-      const newWidth = window.innerWidth;
-      const newHeight = window.innerHeight;
+  // Update camera and renderer on screen resize
+  window.addEventListener('resize', () => {
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
 
-      renderer.setSize(newWidth, newHeight);
-      var aspectRatio = newWidth / newHeight;
+    renderer.setSize(newWidth, newHeight);
+    var aspectRatio = newWidth / newHeight;
 
-      camera.left = -aspectRatio;
-      camera.right = aspectRatio;
-      camera.top = 1;
-      camera.bottom = -1;
+    camera.left = -aspectRatio;
+    camera.right = aspectRatio;
+    camera.top = 1;
+    camera.bottom = -1;
 
-      camera.aspect = newWidth / newHeight;
-      camera.updateProjectionMatrix();
-    });
+    camera.aspect = newWidth / newHeight;
+    camera.updateProjectionMatrix();
+  });
 
-function resetBird(){
-  bird.position.y = 0;
-  bird.position.x = 0;
-}
-
-// Bird setup
-var spriteMaterial = new THREE.SpriteMaterial({ color: 0x32a852 });
-const bird = new THREE.Sprite(spriteMaterial);
-bird.scale.set(0.1, 0.1, 1);
-scene.add(bird);
-
-// Game variables
-let gravity = 0.00036; 
-let velocity = 0;
-let jumpStrength = 0.018;
-
-// Handle user input
-window.addEventListener('keydown', function(event) {
-  if (event.code === 'Space') {
-    velocity = jumpStrength; // Apply jump strength when spacebar is pressed
+  function resetBird(){
+    bird.position.y = 0;
+    bird.position.x = 0;
   }
-});
 
+  // Bird setup
+  var spriteMaterial = new THREE.SpriteMaterial({ color: 0x32a852 });
+  const bird = new THREE.Sprite(spriteMaterial);
+  bird.scale.set(0.1, 0.1, 1);
+  scene.add(bird);
 
-const fontLoader = new FontLoader();
-const textMaterial = new THREE.MeshBasicMaterial({ color: 0x555555 });
+  // Game variables
+  let gravity = 0.00036; 
+  let velocity = 0;
+  let jumpStrength = 0.018;
 
-// Load the Open Sans font
-const movingCubes = [];
-var fontHolder;
-fontLoader.load('src/three/helvetiker_bold.typeface.json', function (font) {
-  fontHolder = font;
-  resetTowers();
-});
+  // Apply jump strength when spacebar is pressed
+  window.addEventListener('keydown', function(event) {
+    if (event.code === 'Space') {
+      velocity = jumpStrength; 
+    }
+  });
 
+var movingCubes = [];
 function resetTowers(){
   movingCubes.splice(0, movingCubes.length);
 
-  for (let row = 0; row < 8; row++) {
-    movingCubes.push(makeCol(row, fontHolder, 2));
+  for (let row = 0; row < questions.length; row++) {
+    movingCubes.push(makeCol(row, 2));
   }
 
   console.log(movingCubes);
@@ -128,11 +122,8 @@ function resetTowers(){
 
 
 const container = document.getElementById('flappy-bird-container');
-function makeCol(row, font, gap){
+function makeCol(row, gap){
   const cubeRow = [];
-  const cubeGeometry = new THREE.BoxGeometry(0.5, 3, -0.1);
-  const cubePassMaterial = new THREE.MeshBasicMaterial({ color: 0x222222 })
-  const cubeFailMaterial = new THREE.MeshBasicMaterial({ color: 0x222222 })
   var spriteMaterial = new THREE.SpriteMaterial({ color: 0x32a89e });
   var clearMaterial = new THREE.SpriteMaterial({ color: 0xffffff, transparent: true, opacity: 0});
 
@@ -142,29 +133,30 @@ function makeCol(row, font, gap){
 
     const labelDiv = document.createElement('div');
     labelDiv.className = 'label';
+    labelDiv.style.opacity = `${opacity}`;
     labelDiv.textContent = (i === 0) ?  shuffled.question: shuffled.answers[i];
     container.appendChild(labelDiv);
+    var spriteType;
 
     if(i==0){
-        var sprite = new THREE.Sprite(clearMaterial.clone());
-        sprite.scale.set(0.2, 0.5, 1);
-        sprite.position.setY(0);
-        sprite.position.setX((row+2)*gap -1);
-        scene.add(sprite);
-        cubeRow.push([sprite, "text", labelDiv]); 
-
+      var sprite = new THREE.Sprite(clearMaterial.clone());
+      sprite.scale.set(0.2, 0.5, 1);
+      sprite.position.setY(0);
+      sprite.position.setX((row+2)*gap -1);
+      spriteType = "text";
     }else{
-        var sprite = new THREE.Sprite((shuffled.answers[i] == shuffled.answer) ?  clearMaterial.clone(): spriteMaterial.clone());
-        sprite.scale.set(0.2, 0.5, 1);
-        sprite.position.setY(-0.75 + 0.5 * (i-1));
-        sprite.position.setX((row+2)*gap);
-        scene.add(sprite);
-      if (shuffled.answers[i] == shuffled.answer){
-        cubeRow.push([sprite, "pass", labelDiv]);  
-      }else{
-        cubeRow.push([sprite, "fail", labelDiv]); 
-      }
+      var sprite = new THREE.Sprite((shuffled.answers[i] == shuffled.answer) ?  clearMaterial.clone(): spriteMaterial.clone());
+      sprite.scale.set(0.2, 0.5, 1);
+      sprite.position.setY(-0.75 + 0.5 * (i-1));
+      sprite.position.setX((row+2)*gap);
+      spriteType = (shuffled.answers[i] == shuffled.answer) ?  "pass": "fail";
     }
+    scene.add(sprite);
+    cubeRow.push({
+      cube: sprite,
+      type: spriteType,
+      label: labelDiv,
+      passed: false});
   }
   return cubeRow;
 }
@@ -187,72 +179,50 @@ function makeCol(row, font, gap){
     }
 
 
-function breakLines(text) {
-    const maxLength = 30;
-    const words = text.split(' ');
-    let currentLine = '';
-    let result = '';
-
-    for (let i = 0; i < words.length; i++) {
-        if ((currentLine + words[i]).length <= maxLength) {
-            currentLine += words[i] + ' ';
-        } else {
-            result += currentLine.trim() + '\n';
-            currentLine = words[i] + ' ';
-        }
+function handleCollsion(){
+  console.log("Collision detected between cube and bird!");
+  for (let row = 0; row < movingCubes.length; row++) {
+    for (let i = 0; i < movingCubes[row].length; i++) {
+      const cube = movingCubes[row][i].cube;
+      scene.remove(cube);
+      movingCubes[row][i].label.parentNode.removeChild(movingCubes[row][i].label);
     }
-
-    // Add the last line
-    result += currentLine.trim();
-
-    return result;
+  }
 }
+
 
 
 // Animation loop
 var overlap = false;
+resetTowers();
 const animate = () => {
   requestAnimationFrame(animate);
 
 
   // Update positions of moving cubes
   for (let row = 0; row < movingCubes.length; row++) {
-    for (let i = 0; i < movingCubes[row].length; i++) {
-      movingCubes[row][i][0].position.x -= 0.01;
+    for (let i = 0; i < ((movingCubes[row].length == 5) ?  5: 0); i++) {
+      movingCubes[row][i].cube.position.x -= 0.01;
 
-      alignHtmlText(movingCubes[row][i][0], movingCubes[row][i][2]);
+      alignHtmlText(movingCubes[row][i].cube, movingCubes[row][i].label);
 
 
-      const cube = movingCubes[row][i][0];
-
-    const cubeBoundingBox = new THREE.Box3().setFromObject(cube);
-    const birdBoundingBox = new THREE.Box3().setFromObject(bird);
-
-    const collisionDetected = cubeBoundingBox.intersectsBox(birdBoundingBox);
-    if (collisionDetected && movingCubes[row][i][1] == "fail") {
-        // Handle collision
-        console.log("Collision detected between cube and bird!");
-        for (let row = 0; row < movingCubes.length; row++) {
-          for (let i = 0; i < movingCubes[row].length; i++) {
-            const cube = movingCubes[row][i][0];
-            scene.remove(cube);
-            console.log(movingCubes[row][i][2])
-            movingCubes[row][i][2].parentNode.removeChild(movingCubes[row][i][2]);
-          }
-        }
-
-        resetTowers();
-        resetBird();
-    }
+      const cube = movingCubes[row][i].cube;
           
-
-
-
-
-
       // Reset cube position if it moves out of the scene
       if (cube.position.x < -16) {
         scene.remove(cube);
+      }
+
+      if(!movingCubes[row][i].passed && movingCubes[row][i].type == "pass" && cube.position.x < bird.position.x){
+        var lowerBound = 0.5 * i - 1.5;
+        var upperBound = 0.5 * (i+1) -1.5;
+          if(!(bird.position.y > lowerBound && bird.position.y < upperBound)){
+            handleCollsion();
+            resetTowers();
+            resetBird();
+          }
+          movingCubes[row][i].passed = true;
       }
     }
   }
@@ -263,8 +233,8 @@ const animate = () => {
 
 
   // Check for collision with ground
-  if (bird.position.y < -6) {
-    bird.position.y = -6;
+  if (bird.position.y < -1) {
+    bird.position.y = -1;
     velocity = 0; // Reset velocity on ground collision
     // Game over logic
     // console.log('Game Over!');
